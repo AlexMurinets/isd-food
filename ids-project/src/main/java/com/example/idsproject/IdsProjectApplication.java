4c9e653a3c8a8cec945586b8407ef28ec3b9cd01
Alexei Murinets <Alexei7a@gmail.com>
2020-08-03 11:25:32 +0300

    Creating project

---
+++ ids-project/src/main/java/com/example/idsproject/IdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.idsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsProjectApplication.class, args);
+	}
+
+}
